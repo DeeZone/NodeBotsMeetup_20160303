5929a4d6acca2abedbdc8bfc0b7f8c0e1ffbf5ac
Darren Dee Lee <darrendouglas.lee@gmail.com>
2016-03-03 19:27:34 -0500

    Exercise 4

---
+++ button.js
@@ -0,0 +1,23 @@
+/**
+ * Fourth exercise of the NodeBots Workshop Meetup
+ * http://acingraham.github.io/reveal.js/#/45
+ */
+ 
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var button = new five.Button(2);
+
+  button.on("press", function() {
+    console.log("Button Pressed!");
+  });
+
+  button.on("hold", function() {
+    console.log("Button Held!");
+  });
+
+  button.on("release", function() {
+    console.log("Button Released!");
+  });
+});
